1b9d9970fb4771ff351d3394642975054fde02b8
heonhag <k01050713651@gmail.com>
2023-09-05 20:08:16 +0900

    add count.cc

--- count.cc
+++ count.cc
@@ -3,4 +3,11 @@
 void count_number() {
     // lab2 //
     // do it yourself //
+    int i;
+    for(int i = 1; i <= 10; i++){
+	    std::cout << i << std::endl;
+    }
+    std::cout << "2019313357" << std::endl;
+
+    return;
 }
